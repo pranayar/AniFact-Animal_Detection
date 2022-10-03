0ea5b917498b148b216f36ee58558c4052e56163
pranayar <68011334+pranayar@users.noreply.github.com>
2022-10-04 00:16:39 +0530

    bhbuu

--- app/src/main/java/com/example/imgp/MainActivity.java
+++ app/src/main/java/com/example/imgp/MainActivity.java
@@ -8,11 +8,13 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.imgp.ml.MobilenetAnimals;
 import com.example.imgp.ml.Model;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -22,6 +24,7 @@ import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,17 +48,6 @@ public class MainActivity extends AppCompatActivity {
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, 100);
-       /* select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, 100);
-
-            }
-        });
-        */
 
         info = (Button)findViewById(R.id.info);
 
@@ -73,48 +65,86 @@ public class MainActivity extends AppCompatActivity {
         predict.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                img=Bitmap.createScaledBitmap(img,224, 224,true);
+                img=Bitmap.createScaledBitmap(img,72, 72,true);
 
+//                try {
+//                    Model model = Model.newInstance(getApplicationContext());
+//
+//                    // Creates inputs for reference.
+//                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 144, 144, 3}, DataType.UINT8);
+//                    TensorImage tensorImage=new TensorImage(DataType.UINT8);
+//                    tensorImage.load(img);
+//                    ByteBuffer byteBuffer=tensorImage.getBuffer();
+//                    inputFeature0.loadBuffer(byteBuffer);
+//
+//                    // Runs model inference and gets result.
+//                    Model.Outputs outputs = model.process(inputFeature0);
+//
+//                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+//                    /*if(outputFeature0.getFloatArray()[0]>0)
+//                   tv.setText("wolf");
+//                    else
+//                        tv.setText("none");
+//                    // Releases model resources if no longer used.
+//
+//                     */
+//                    float f[]=new float[9];
+//                    f[0]=outputFeature0.getFloatArray()[0];
+//                    f[1]=outputFeature0.getFloatArray()[1];
+//                    f[2]=outputFeature0.getFloatArray()[2];
+//                    f[3]=outputFeature0.getFloatArray()[3];
+//                    f[4]=outputFeature0.getFloatArray()[4];
+//                    f[5]=outputFeature0.getFloatArray()[5];
+//                    f[6]=outputFeature0.getFloatArray()[6];
+//                    f[7]=outputFeature0.getFloatArray()[7];
+//                    f[8]=outputFeature0.getFloatArray()[8];
+//                    //f[9]=outputFeature0.getFloatArray()[9];
+//                    Log.i("F is ",Arrays.toString(f));
+//                    String[] s={"Bull","Butterfly","Cats","Chicken","Dogs","Elephant","Horse","Sheep","Squirrel"};
+//                    //animal_names = {'Bull': 0,'Butterfly': 1,'Cats': 2,'Chicken': 3,'Dogs': 4,'Elephant': 5,'Horse': 6,'Sheep': 7,'Squirrel': 8}
+//                    // String[] s={"wolf","elephant","giraffe","deer","camel","cat","cow","kangaroo","dog","pig"};
+//               int ind=indexOf(f,largest(f));
+//
+//               common=s[ind];
+//               tv.setText(common);
+//                    model.close();
+//
+//                } catch (IOException e) {
+//                    // TODO Handle the exception
+//                }
                 try {
-                    Model model = Model.newInstance(getApplicationContext());
+                    MobilenetAnimals model = MobilenetAnimals.newInstance(getApplicationContext());
 
                     // Creates inputs for reference.
-                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.UINT8);
-                    TensorImage tensorImage=new TensorImage(DataType.UINT8);
-                    tensorImage.load(img);
-                    ByteBuffer byteBuffer=tensorImage.getBuffer();
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 72, 72, 3}, DataType.FLOAT32);
+                    ByteBuffer byteBuffer= ByteBuffer.allocateDirect(4*72*72*3);
+                    byteBuffer.order(ByteOrder.nativeOrder());
+                    int[] intvalues=new int[72*72];
+                    img.getPixels(intvalues,0,img.getWidth(),0,0,img.getWidth(),img.getHeight());
+                    int pixel=0;
+                    for(int i=0;i<72;i++)
+                    {
+                        for(int j=0;j<72;j++) {
+                        int val=intvalues[pixel++];
+                            byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f /255));
+                            byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f/255));
+                            byteBuffer.putFloat((val & 0xFF) * (1.f/255));
+                        }
+                    }
                     inputFeature0.loadBuffer(byteBuffer);
 
                     // Runs model inference and gets result.
-                    Model.Outputs outputs = model.process(inputFeature0);
-
+                    MobilenetAnimals.Outputs outputs = model.process(inputFeature0);
                     TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-                    /*if(outputFeature0.getFloatArray()[0]>0)
-                   tv.setText("wolf");
-                    else
-                        tv.setText("none");
+                    float[] confidence=outputFeature0.getFloatArray();
+                    float temp=largest(confidence);
+                    String[] s={"Bull","Butterfly","Cat","Chicken","Dog","Elephant","Horse","Sheep","Squirrel"};
+                    common=s[indexOf(confidence,temp)];
+                    Log.i("max is ", common);
+                    //animal_names = {'Bull': 0,'Butterfly': 1,'Cats': 2,'Chicken': 3,'Dogs': 4,'Elephant': 5,'Horse': 6,'Sheep': 7,'Squirrel': 8}
                     // Releases model resources if no longer used.
-
-                     */
-                     float f[]=new float[10];
-                    f[0]=outputFeature0.getFloatArray()[0];
-                    f[1]=outputFeature0.getFloatArray()[1];
-                    f[2]=outputFeature0.getFloatArray()[2];
-                    f[3]=outputFeature0.getFloatArray()[3];
-                    f[4]=outputFeature0.getFloatArray()[4];
-                    f[5]=outputFeature0.getFloatArray()[5];
-                    f[6]=outputFeature0.getFloatArray()[6];
-                    f[7]=outputFeature0.getFloatArray()[7];
-                    f[8]=outputFeature0.getFloatArray()[8];
-                    f[9]=outputFeature0.getFloatArray()[9];
-
-                    String[] s={"wolf","elephant","giraffe","deer","camel","cat","cow","kangaroo","dog","pig"};
-               int ind=indexOf(f,largest(f));
-
-               common=s[ind];
-               tv.setText(common);
+                    tv.setText(common);
                     model.close();
-
                 } catch (IOException e) {
                     // TODO Handle the exception
                 }
